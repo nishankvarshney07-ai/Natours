61403044e1b4bebd0d736a0e7da1d72384ef0ee5
Nishank <nishankvarshney07@gmail.com>
2026-07-24 15:35:02 +0530

    Restore usercontroller, tourroutes, userroutes content

---
+++ server.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`app running on port ${port}....`);
+});
